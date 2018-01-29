2a854e91b14021846f7f674592f2918390d83b4d
Потапов Максим <potapovm89@gmail.com>
2018-01-29 17:04:19 +0300

    IS-495: Интерфейс плагина для DemandProducer'a

---
+++ src/bgTeam.Core.Esb/ProduceMessages/IDemandPlugin.cs
@@ -0,0 +1,15 @@
+﻿namespace bgTeam.Esb.ProduceMessages
+{
+    using bgTeam.ProduceMessages;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IDemandPlugin : IDictionaryConfig
+    {
+        string ConfigName { get; }
+
+        IEnumerable<dynamic> Execute(Dictionary<string, object> map);
+
+        Task<IEnumerable<dynamic>> ExecuteAsync(Dictionary<string, object> map);
+    }
+}
