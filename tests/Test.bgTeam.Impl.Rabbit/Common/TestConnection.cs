f274a6249effcf4d13fcf62d11deda852286fc50
alexey bugrov <bugrovap@trcont.ru>
2018-03-02 14:23:25 +0300

    Test.bgTeam.Impl.Rabbit

---
+++ tests/Test.bgTeam.Impl.Rabbit/Common/TestConnection.cs
@@ -0,0 +1,363 @@
+﻿using bgTeam.Queues;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.bgTeam.Impl.Rabbit.Common
+{
+    public class TestConnection : IConnection
+    {
+        public bool AutoClose { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ushort ChannelMax => throw new NotImplementedException();
+
+        public IDictionary<string, object> ClientProperties => throw new NotImplementedException();
+
+        public ShutdownEventArgs CloseReason => throw new NotImplementedException();
+
+        public AmqpTcpEndpoint Endpoint => throw new NotImplementedException();
+
+        public uint FrameMax => throw new NotImplementedException();
+
+        public ushort Heartbeat => throw new NotImplementedException();
+
+        public bool IsOpen => throw new NotImplementedException();
+
+        public AmqpTcpEndpoint[] KnownHosts => throw new NotImplementedException();
+
+        public IProtocol Protocol => throw new NotImplementedException();
+
+        public IDictionary<string, object> ServerProperties => throw new NotImplementedException();
+
+        public IList<ShutdownReportEntry> ShutdownReport => throw new NotImplementedException();
+
+        public string ClientProvidedName => throw new NotImplementedException();
+
+        public ConsumerWorkService ConsumerWorkService => throw new NotImplementedException();
+
+        public int LocalPort => throw new NotImplementedException();
+
+        public int RemotePort => throw new NotImplementedException();
+
+        public event EventHandler<CallbackExceptionEventArgs> CallbackException;
+        public event EventHandler<EventArgs> RecoverySucceeded;
+        public event EventHandler<ConnectionRecoveryErrorEventArgs> ConnectionRecoveryError;
+        public event EventHandler<ConnectionBlockedEventArgs> ConnectionBlocked;
+        public event EventHandler<ShutdownEventArgs> ConnectionShutdown;
+        public event EventHandler<EventArgs> ConnectionUnblocked;
+
+        public void Abort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Abort(ushort reasonCode, string reasonText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Abort(int timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Abort(ushort reasonCode, string reasonText, int timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close(ushort reasonCode, string reasonText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close(int timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close(ushort reasonCode, string reasonText, int timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IModel CreateModel()
+        {
+            return new TestModel();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void HandleConnectionBlocked(string reason)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleConnectionUnblocked()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TestModel : IModel
+    {
+        public int ChannelNumber => throw new NotImplementedException();
+
+        public ShutdownEventArgs CloseReason => throw new NotImplementedException();
+
+        public IBasicConsumer DefaultConsumer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool IsClosed => throw new NotImplementedException();
+
+        public bool IsOpen => throw new NotImplementedException();
+
+        public ulong NextPublishSeqNo => throw new NotImplementedException();
+
+        public TimeSpan ContinuationTimeout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler<BasicAckEventArgs> BasicAcks;
+        public event EventHandler<BasicNackEventArgs> BasicNacks;
+        public event EventHandler<EventArgs> BasicRecoverOk;
+        public event EventHandler<BasicReturnEventArgs> BasicReturn;
+        public event EventHandler<CallbackExceptionEventArgs> CallbackException;
+        public event EventHandler<FlowControlEventArgs> FlowControl;
+        public event EventHandler<ShutdownEventArgs> ModelShutdown;
+
+        public void Abort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Abort(ushort replyCode, string replyText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BasicAck(ulong deliveryTag, bool multiple)
+        {
+        }
+
+        public void BasicCancel(string consumerTag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BasicConsume(string queue, bool autoAck, string consumerTag, bool noLocal, bool exclusive, IDictionary<string, object> arguments, IBasicConsumer consumer)
+        {
+            throw new NotImplementedException();
+        }
+
+        IMessageProvider msgprovider = new MessageProviderDefault();
+
+        public BasicGetResult BasicGet(string queue, bool autoAck)
+        {
+            var bt = msgprovider.PrepareMessageByte(new { Id = 1, name = "test", });
+
+            return new BasicGetResult(0, true, "", "", 0, null, bt);
+        }
+
+        public void BasicNack(ulong deliveryTag, bool multiple, bool requeue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BasicPublish(string exchange, string routingKey, bool mandatory, IBasicProperties basicProperties, byte[] body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BasicQos(uint prefetchSize, ushort prefetchCount, bool global)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BasicRecover(bool requeue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BasicRecoverAsync(bool requeue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BasicReject(ulong deliveryTag, bool requeue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close(ushort replyCode, string replyText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmSelect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint ConsumerCount(string queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBasicProperties CreateBasicProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void ExchangeBind(string destination, string source, string routingKey, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeBindNoWait(string destination, string source, string routingKey, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeDeclare(string exchange, string type, bool durable, bool autoDelete, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeDeclareNoWait(string exchange, string type, bool durable, bool autoDelete, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeDeclarePassive(string exchange)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeDelete(string exchange, bool ifUnused)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeDeleteNoWait(string exchange, bool ifUnused)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeUnbind(string destination, string source, string routingKey, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeUnbindNoWait(string destination, string source, string routingKey, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint MessageCount(string queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void QueueBind(string queue, string exchange, string routingKey, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void QueueBindNoWait(string queue, string exchange, string routingKey, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueueDeclareOk QueueDeclare(string queue, bool durable, bool exclusive, bool autoDelete, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void QueueDeclareNoWait(string queue, bool durable, bool exclusive, bool autoDelete, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueueDeclareOk QueueDeclarePassive(string queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint QueueDelete(string queue, bool ifUnused, bool ifEmpty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void QueueDeleteNoWait(string queue, bool ifUnused, bool ifEmpty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint QueuePurge(string queue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void QueueUnbind(string queue, string exchange, string routingKey, IDictionary<string, object> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TxCommit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TxRollback()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TxSelect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WaitForConfirms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WaitForConfirms(TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WaitForConfirms(TimeSpan timeout, out bool timedOut)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WaitForConfirmsOrDie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WaitForConfirmsOrDie(TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
