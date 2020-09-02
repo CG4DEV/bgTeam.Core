e142a3f6e0d97b9584e110a3518647c9f7fe9c0f
Bugrov Alexey <bugrovalexey@gmail.com>
2020-09-02 13:37:52 +0400

    update bgTeam.ProjectTemplate

---
+++ src/bgTeam.ProjectTemplate/FileGenerators/ProjectInfoItem.cs
@@ -0,0 +1,51 @@
+﻿namespace bgTeam.ProjectTemplate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using bgTeam.Extensions;
+
+    internal class ProjectInfoItem
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public bool Build { get; set; }
+
+        public ProjectTypeEnum Type { get; set; }
+
+        public string Code { get; set; }
+
+        public string Description { get; set; }
+
+        public ProjectTypeEnum Parent { get; private set; }
+
+        public IList<string> ListChild { get; private set; }
+
+        public ProjectInfoItem(string name, string path, ProjectTypeEnum type = ProjectTypeEnum.Compile)
+        {
+            Name = name;
+            Path = path;
+            Type = type;
+
+            if (type == ProjectTypeEnum.Compile || type == ProjectTypeEnum.Tests)
+            {
+                Build = true;
+            }
+
+            Code = Guid.NewGuid().ToString().ToUpperInvariant();
+
+            ListChild = new List<string>();
+        }
+
+        public void AddChild(ProjectInfoItem project)
+        {
+            ListChild.Add(project.Code);
+        }
+    }
+}
