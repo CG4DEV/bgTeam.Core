196aaddb78148bf3fa4cfb3afe0d9b439c480666
Ilya Belyanskiy <ilya.belyanskiy@pm.bet>
2020-10-01 17:54:16 +0400

    Template updating, part 1

--- examples/bgTeam.ProjectTemplate/FileGenerators/SolutionGenerator.cs
+++ examples/bgTeam.ProjectTemplate/FileGenerators/SolutionGenerator.cs
@@ -1,6 +1,5 @@
 ﻿namespace bgTeam.ProjectTemplate
 {
-    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
@@ -10,6 +9,8 @@
 
     internal class SolutionGenerator
     {
+        private readonly char _separator = Path.DirectorySeparatorChar;
+
         public void Generate(string @namespace, string name, SolutionSettings settings)
         {
             var folder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "result");
@@ -25,7 +26,7 @@
 
             var file = ProjectsFile(projects);
 
-            File.WriteAllText($"{folder}{Path.DirectorySeparatorChar}{nameSln}.sln", file);
+            File.WriteAllText($"{folder}{_separator}{nameSln}.sln", file);
 
             File.Copy("./Resourse/LICENSE", $"{folder}/LICENSE");
             File.Copy("./Resourse/README.md", $"{folder}/README.md");
@@ -45,19 +46,12 @@
                 Directory.CreateDirectory(Path.GetDirectoryName(dest));
                 File.Copy(item, dest);
             }
-
-            foreach (var item in Directory.EnumerateFiles("./Resourse/wiki-generator", "*", SearchOption.AllDirectories))
-            {
-                var dest = $"{folder}/{Path.GetRelativePath("./Resourse/", item)}";
-                Directory.CreateDirectory(Path.GetDirectoryName(dest));
-                File.Copy(item, dest);
-            }
         }
 
         private IEnumerable<ProjectInfoItem> GenerateProjects(string name, string folder, string path, SolutionSettings settings)
         {
             var result = new List<ProjectInfoItem>();
-            var fullPath = $"{folder}{Path.DirectorySeparatorChar}{path}";
+            var fullPath = $"{folder}{_separator}{path}";
 
             var fapps = new ProjectInfoItem("Apps", $"Apps", ProjectTypeEnum.Folder);
             var fmain = new ProjectInfoItem("Main", $"Main", ProjectTypeEnum.Folder);
@@ -79,16 +73,16 @@
             var p1 = new ProjectGenerator($"{name}.Common", fullPath);
             p1.ProjectFile(new[] { ("bgTeam.Core", settings.BgTeamVersion) });
             p1.Folder("Impl");
-            p1.ClassTemplateFile("ITestService", $"Common{Path.DirectorySeparatorChar}ITestService");
-            p1.ClassTemplateFile("TestService", $"Common{Path.DirectorySeparatorChar}TestService", new[] { "Impl" });
+            p1.ClassTemplateFile("ITestService", $"Common{_separator}ITestService");
+            p1.ClassTemplateFile("TestService", $"Common{_separator}Impl{_separator}TestService", new[] { "Impl" });
 
             if (settings.IsWeb)
             {
                 p1.Folder("Exceptions");
-                p1.ClassTemplateFile("BadRequestException", $"Common{Path.DirectorySeparatorChar}BadRequestException", new[] { "Exceptions" });
+                p1.ClassTemplateFile("BadRequestException", $"Common{_separator}BadRequestException", new[] { "Exceptions" });
             }
 
-            var fp1 = new ProjectInfoItem(p1.Name, $"{path}{Path.DirectorySeparatorChar}{p1.Output}");
+            var fp1 = new ProjectInfoItem(p1.Name, $"{path}{_separator}{p1.Output}");
 
             var p2 = new ProjectGenerator($"{name}.DataAccess", fullPath);
             p2.ProjectFile(
@@ -96,20 +90,23 @@
                 {
                     ("bgTeam.Core", settings.BgTeamVersion),
                     ("bgTeam.DataAccess", settings.BgTeamVersion),
+                    ("Microsoft.EntityFrameworkCore", settings.EntityFrameworkCoreVersion),
+                    ("Microsoft.EntityFrameworkCore.Proxies", settings.EntityFrameworkCoreVersion),
+                    ("Npgsql.EntityFrameworkCore.PostgreSQL", settings.EntityFrameworkCoreNpgsqlVersion),
                 }, projects: new[] { $"{name}.Domain" });
             p2.Folder("Impl");
-            p2.ClassTemplateFile("TestQuery", $"DataAccess{Path.DirectorySeparatorChar}TestQuery", replist: new List<(string, string)> { ("$prj$", name) });
-            p2.ClassTemplateFile("TestQueryContext", $"DataAccess{Path.DirectorySeparatorChar}TestQueryContext");
-            var fp2 = new ProjectInfoItem(p2.Name, $"{path}{Path.DirectorySeparatorChar}{p2.Output}");
+            p2.ClassTemplateFile("TestQuery", $"DataAccess{_separator}TestQuery", replist: new List<(string, string)> { ("$prj$", name) });
+            p2.ClassTemplateFile("TestQueryContext", $"DataAccess{_separator}TestQueryContext");
+            var fp2 = new ProjectInfoItem(p2.Name, $"{path}{_separator}{p2.Output}");
 
             var p3 = new ProjectGenerator($"{name}.Domain", fullPath);
             p3.ProjectFile(new[] { ("bgTeam.Impl.Dapper", settings.BgTeamVersion) });
             p3.Folder("Dto");
             p3.Folder("Entities");
-            p3.ClassTemplateFile("IEntity", $"Domain{Path.DirectorySeparatorChar}IEntity");
-            p3.ClassTemplateFile("Test", $"Domain{Path.DirectorySeparatorChar}Test", new[] { "Entities" });
-            p3.ClassTemplateFile("TestDto", $"Domain{Path.DirectorySeparatorChar}TestDto", new[] { "Dto" });
-            var fp3 = new ProjectInfoItem(p3.Name, $"{path}{Path.DirectorySeparatorChar}{p3.Output}");
+            p3.ClassTemplateFile("IEntity", $"Domain{_separator}IEntity");
+            p3.ClassTemplateFile("Test", $"Domain{_separator}Test", new[] { "Entities" });
+            p3.ClassTemplateFile("TestDto", $"Domain{_separator}Dto{_separator}TestDto", new[] { "Dto" });
+            var fp3 = new ProjectInfoItem(p3.Name, $"{path}{_separator}{p3.Output}");
 
             var p4 = new ProjectGenerator($"{name}.Story", fullPath);
             p4.ProjectFile(
@@ -118,12 +115,12 @@
                     ("bgTeam.Core", settings.BgTeamVersion),
                     ("bgTeam.DataAccess", settings.BgTeamVersion),
                 }, projects: new[] { $"{name}.Domain" });
-            p4.ClassTemplateFile("TestStory", $"Story{Path.DirectorySeparatorChar}TestStory", replist: new List<(string, string)> { ("$prj$", name) });
-            p4.ClassTemplateFile("TestStoryContext", $"Story{Path.DirectorySeparatorChar}TestStoryContext");
-            p4.ClassTemplateFile("IStoryLibrary", $"Story{Path.DirectorySeparatorChar}IStoryLibrary");
-            var fp4 = new ProjectInfoItem(p4.Name, $"{path}{Path.DirectorySeparatorChar}{p4.Output}");
+            p4.ClassTemplateFile("TestStory", $"Story{_separator}TestStory", replist: new List<(string, string)> { ("$prj$", name) });
+            p4.ClassTemplateFile("TestStoryContext", $"Story{_separator}TestStoryContext");
+            p4.ClassTemplateFile("IStoryLibrary", $"Story{_separator}IStoryLibrary");
+            var fp4 = new ProjectInfoItem(p4.Name, $"{path}{_separator}{p4.Output}");
 
-            var p5 = new ProjectGenerator($"{name}.Tests", $"{folder}{Path.DirectorySeparatorChar}tests");
+            var p5 = new ProjectGenerator($"{name}.Tests", $"{folder}{_separator}tests");
             p5.ProjectFile(
                 new[]
                 {
@@ -134,12 +131,12 @@
                 }, projects: new[] { $"{name}.Story" },
                 configs: true);
             p5.Folder("Common");
-            p5.ClassTemplateFile("TestStoryTests", $"Tests{Path.DirectorySeparatorChar}TestStoryTests", replist: new List<(string, string)> { ("$prj$", name) });
-            p5.ClassTemplateFile("FactoryTestService", $"Tests{Path.DirectorySeparatorChar}FactoryTestService", new[] { "Common" });
-            p5.JsonTemplateFile("appsettings", $"Tests{Path.DirectorySeparatorChar}appsettings");
-            p5.JsonTemplateFile("appsettings.Development", $"Web{Path.DirectorySeparatorChar}appsettings");
-            p5.JsonTemplateFile("appsettings.Production", $"Web{Path.DirectorySeparatorChar}appsettings");
-            var fp5 = new ProjectInfoItem(p5.Name, $"tests{Path.DirectorySeparatorChar}{p5.Output}", ProjectTypeEnum.Compile);
+            p5.ClassTemplateFile("TestStoryTests", $"Tests{_separator}TestStoryTests", replist: new List<(string, string)> { ("$prj$", name) });
+            p5.ClassTemplateFile("FactoryTestService", $"Tests{_separator}FactoryTestService", new[] { "Common" });
+            p5.JsonTemplateFile("appsettings", $"Tests{_separator}appsettings");
+            p5.JsonTemplateFile("appsettings.Development", $"Web{_separator}appsettings");
+            p5.JsonTemplateFile("appsettings.Production", $"Web{_separator}appsettings");
+            var fp5 = new ProjectInfoItem(p5.Name, $"tests{_separator}{p5.Output}", ProjectTypeEnum.Compile);
 
             fmain.AddChild(fp1);
             fmain.AddChild(fp2);
@@ -174,16 +171,16 @@
                     }, type: "Exe",
                     projects: new[] { $"{name}.Story" },
                     configs: true);
-                p6.ClassTemplateFile("AppSettings", $"App{Path.DirectorySeparatorChar}AppSettings");
-                p6.ClassTemplateFile("AppIocConfigure", $"App{Path.DirectorySeparatorChar}AppIocConfigure", replist: new List<(string, string)> { ("$prj$", name) });
-                p6.ClassTemplateFile("Program", $"App{Path.DirectorySeparatorChar}Program", replist: new List<(string, string)> { ("$prj$", name) });
-                p6.ClassTemplateFile("Runner", $"App{Path.DirectorySeparatorChar}Runner", replist: new List<(string, string)> { ("$prj$", name) });
-                p6.JsonTemplateFile("appsettings", $"App{Path.DirectorySeparatorChar}appsettings");
-                p6.JsonTemplateFile("appsettings.Development", $"App{Path.DirectorySeparatorChar}appsettings");
-                p6.JsonTemplateFile("appsettings.Production", $"App{Path.DirectorySeparatorChar}appsettings");
+                p6.ClassTemplateFile("AppSettings", $"App{_separator}AppSettings");
+                p6.ClassTemplateFile("AppIocConfigure", $"App{_separator}AppIocConfigure", replist: new List<(string, string)> { ("$prj$", name) });
+                p6.ClassTemplateFile("Program", $"App{_separator}Program", replist: new List<(string, string)> { ("$prj$", name) });
+                p6.ClassTemplateFile("Runner", $"App{_separator}Runner", replist: new List<(string, string)> { ("$prj$", name) });
+                p6.JsonTemplateFile("appsettings", $"App{_separator}appsettings");
+                p6.JsonTemplateFile("appsettings.Development", $"App{_separator}appsettings");
+                p6.JsonTemplateFile("appsettings.Production", $"App{_separator}appsettings");
                 p6.Folder("Properties");
-                p6.JsonTemplateFile("launchSettings", $"App{Path.DirectorySeparatorChar}launchSettings", new[] { "Properties" }, new List<(string, string)> { ("$prj$", p6.Name) });
-                var fp6 = new ProjectInfoItem(p6.Name, $"{path}{Path.DirectorySeparatorChar}{p6.Output}");
+                p6.JsonTemplateFile("launchSettings", $"App{_separator}launchSettings", new[] { "Properties" }, new List<(string, string)> { ("$prj$", p6.Name) });
+                var fp6 = new ProjectInfoItem(p6.Name, $"{path}{_separator}{p6.Output}");
                 //fmain.AddChild(fp6);
                 fapps.AddChild(fp6);
                 result.Add(fp6);
@@ -201,13 +198,13 @@
                         ("Microsoft.AspNetCore.App", "2.2.8"),
                         ("Scrutor", "3.2.2"),
                     }, projects: new[] { $"{name}.Common", $"{name}.DataAccess", $"{name}.Story" });
-                p7.ClassTemplateFile("AppSettings", $"App{Path.DirectorySeparatorChar}AppSettings");
-                p7.ClassTemplateFile("AppIocConfigure", $"WebApp{Path.DirectorySeparatorChar}AppIocConfigure", replist: new List<(string, string)> { ("$prj$", name) });
-                p7.ClassTemplateFile("AppMiddlewareException", $"WebApp{Path.DirectorySeparatorChar}AppMiddlewareException", replist: new List<(string, string)> { ("$prj$", name) });
+                p7.ClassTemplateFile("AppSettings", $"App{_separator}AppSettings");
+                p7.ClassTemplateFile("AppIocConfigure", $"WebApp{_separator}AppIocConfigure", replist: new List<(string, string)> { ("$prj$", name) });
+                p7.ClassTemplateFile("AppMiddlewareException", $"WebApp{_separator}AppMiddlewareException", replist: new List<(string, string)> { ("$prj$", name) });
                 p7.Folder("Controllers");
-                p7.ClassTemplateFile("HomeController", $"WebApp{Path.DirectorySeparatorChar}Controllers{Path.DirectorySeparatorChar}HomeController", new[] { "Controllers" }, new List<(string, string)> { ("$api-name$", $"{name} API") });
-                p7.ClassTemplateFile("UserController", $"WebApp{Path.DirectorySeparatorChar}Controllers{Path.DirectorySeparatorChar}UserController", new[] { "Controllers" });
-                var fp7 = new ProjectInfoItem(p7.Name, $"{path}{Path.DirectorySeparatorChar}{p7.Output}");
+                p7.ClassTemplateFile("HomeController", $"WebApp{_separator}Controllers{_separator}HomeController", new[] { "Controllers" }, new List<(string, string)> { ("$api-name$", $"{name} API") });
+                p7.ClassTemplateFile("UserController", $"WebApp{_separator}Controllers{_separator}UserController", new[] { "Controllers" });
+                var fp7 = new ProjectInfoItem(p7.Name, $"{path}{_separator}{p7.Output}");
                 fmain.AddChild(fp7);
                 result.Add(fp7);
 
@@ -222,15 +219,15 @@
                     "Exe",
                     new[] { $"{name}.WebApp" },
                     true);
-                p8.ClassTemplateFile("Program", $"Web{Path.DirectorySeparatorChar}Program", replist: new List<(string, string)> { ("$prj$", name) });
-                p8.ClassTemplateFile("Startup", $"Web{Path.DirectorySeparatorChar}Startup", replist: new List<(string, string)> { ("$prj$", name), ("$api-name$", $"{name} API") });
-                p8.JsonTemplateFile("appsettings", $"Web{Path.DirectorySeparatorChar}appsettings");
-                p8.JsonTemplateFile("appsettings.Development", $"Web{Path.DirectorySeparatorChar}appsettings");
-                p8.JsonTemplateFile("appsettings.Production", $"Web{Path.DirectorySeparatorChar}appsettings");
+                p8.ClassTemplateFile("Program", $"Web{_separator}Program", replist: new List<(string, string)> { ("$prj$", name) });
+                p8.ClassTemplateFile("Startup", $"Web{_separator}Startup", replist: new List<(string, string)> { ("$prj$", name), ("$api-name$", $"{name} API") });
+                p8.JsonTemplateFile("appsettings", $"Web{_separator}appsettings");
+                p8.JsonTemplateFile("appsettings.Development", $"Web{_separator}appsettings");
+                p8.JsonTemplateFile("appsettings.Production", $"Web{_separator}appsettings");
                 p8.Folder("Properties");
-                p8.JsonTemplateFile("launchSettings", $"Web{Path.DirectorySeparatorChar}launchSettings", new[] { "Properties" }, new List<(string, string)> { ("$prj$", p8.Name) });
+                p8.JsonTemplateFile("launchSettings", $"Web{_separator}launchSettings", new[] { "Properties" }, new List<(string, string)> { ("$prj$", p8.Name) });
 
-                var fp8 = new ProjectInfoItem(p8.Name, $"{path}{Path.DirectorySeparatorChar}{p8.Output}");
+                var fp8 = new ProjectInfoItem(p8.Name, $"{path}{_separator}{p8.Output}");
                 //fmain.AddChild(fp8);
                 fapps.AddChild(fp8);
                 result.Add(fp8);
@@ -249,14 +246,14 @@
                 type: "Exe",
                 projects: new[] { $"{name}.Story" },
                 configs: true);
-            p9.ClassTemplateFile("AppIocConfigure", $"StoryRunner{Path.DirectorySeparatorChar}AppIocConfigure");
-            p9.ClassTemplateFile("AppSettings", $"StoryRunner{Path.DirectorySeparatorChar}AppSettings");
-            p9.ClassTemplateFile("Program", $"StoryRunner{Path.DirectorySeparatorChar}Program");
-            p9.ClassTemplateFile("Runner", $"StoryRunner{Path.DirectorySeparatorChar}Runner");
-            p9.JsonTemplateFile("appsettings", $"StoryRunner{Path.DirectorySeparatorChar}appsettings");
-            p9.JsonTemplateFile("appsettings.Development", $"StoryRunner{Path.DirectorySeparatorChar}appsettings");
-            p9.JsonTemplateFile("appsettings.Production", $"StoryRunner{Path.DirectorySeparatorChar}appsettings");
-            var fp9 = new ProjectInfoItem(p9.Name, $"{path}{Path.DirectorySeparatorChar}{p9.Output}");
+            p9.ClassTemplateFile("AppIocConfigure", $"StoryRunner{_separator}AppIocConfigure");
+            p9.ClassTemplateFile("AppSettings", $"StoryRunner{_separator}AppSettings");
+            p9.ClassTemplateFile("Program", $"StoryRunner{_separator}Program");
+            p9.ClassTemplateFile("Runner", $"StoryRunner{_separator}Runner");
+            p9.JsonTemplateFile("appsettings", $"StoryRunner{_separator}appsettings");
+            p9.JsonTemplateFile("appsettings.Development", $"StoryRunner{_separator}appsettings");
+            p9.JsonTemplateFile("appsettings.Production", $"StoryRunner{_separator}appsettings");
+            var fp9 = new ProjectInfoItem(p9.Name, $"{path}{_separator}{p9.Output}");
             fsrv.AddChild(fp9);
             result.Add(fp9);
 
@@ -273,16 +270,16 @@
                 },
                 type: "Exe",
                 configs: true);
-            p10.ClassTemplateFile("AppIocConfigure", $"StoryScheduler{Path.DirectorySeparatorChar}AppIocConfigure");
-            p10.ClassTemplateFile("AppSettings", $"StoryScheduler{Path.DirectorySeparatorChar}AppSettings");
-            p10.ClassTemplateFile("Program", $"StoryScheduler{Path.DirectorySeparatorChar}Program");
-            p10.ClassTemplateFile("Runner", $"StoryScheduler{Path.DirectorySeparatorChar}Runner");
-            p10.JsonTemplateFile("appsettings", $"StoryScheduler{Path.DirectorySeparatorChar}appsettings");
-            p10.JsonTemplateFile("appsettings.Development", $"StoryScheduler{Path.DirectorySeparatorChar}appsettings");
-            p10.JsonTemplateFile("appsettings.Production", $"StoryScheduler{Path.DirectorySeparatorChar}appsettings");
+            p10.ClassTemplateFile("AppIocConfigure", $"StoryScheduler{_separator}AppIocConfigure");
+            p10.ClassTemplateFile("AppSettings", $"StoryScheduler{_separator}AppSettings");
+            p10.ClassTemplateFile("Program", $"StoryScheduler{_separator}Program");
+            p10.ClassTemplateFile("Runner", $"StoryScheduler{_separator}Runner");
+            p10.JsonTemplateFile("appsettings", $"StoryScheduler{_separator}appsettings");
+            p10.JsonTemplateFile("appsettings.Development", $"StoryScheduler{_separator}appsettings");
+            p10.JsonTemplateFile("appsettings.Production", $"StoryScheduler{_separator}appsettings");
             p10.Folder("Configurations");
-            p10.JsonTemplateFile("0_Examples", $"StoryScheduler{Path.DirectorySeparatorChar}0_Examples", new[] { "Configurations" });
-            var fp10 = new ProjectInfoItem(p10.Name, $"{path}{Path.DirectorySeparatorChar}{p10.Output}");
+            p10.JsonTemplateFile("0_Examples", $"StoryScheduler{_separator}0_Examples", new[] { "Configurations" });
+            var fp10 = new ProjectInfoItem(p10.Name, $"{path}{_separator}{p10.Output}");
             fsrv.AddChild(fp10);
             result.Add(fp10);
 
